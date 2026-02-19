fd32e81eae17a984fa0b5aadbc558d0c20b15413
thuraichamyvithushan <145326510+thuraichamyvithushan@users.noreply.github.com>
2026-02-19 13:05:16 +0530

    v4

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = import.meta.env.VITE_API_URL || (
     window.location.hostname === 'localhost'
         ? 'http://localhost:5001'
-        : 'https://staff-purchase-form.vercel.app'
+        : 'https://staffpurchaseaus.hointernal.com'
 );
